8d39374426a23b6c0e50c464a218634028103065
Ilya Spirkin <spirkinilya@gmail.com>
2024-01-03 21:06:14 +0300

    feat: getIndexOf

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -163,8 +163,15 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  let index = -1;
+  for (let i = 0; i < str.length; i += 1) {
+    if (letter === str[i]) {
+      index = i;
+      break;
+    }
+  }
+  return index;
 }
 
 /**
